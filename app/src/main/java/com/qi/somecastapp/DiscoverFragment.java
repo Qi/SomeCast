39fdccb651fa7781f55465391de78162e81bbb28
Qi Wu <wuqi.vortex@gmail.com>
2018-11-22 11:07:46 -0500

    Discover Fragment clean up

--- app/src/main/java/com/qi/somecastapp/DiscoverFragment.java
+++ app/src/main/java/com/qi/somecastapp/DiscoverFragment.java
@@ -1,24 +1,98 @@
 package com.qi.somecastapp;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+import com.qi.somecastapp.utilities.JsonUtils;
+import com.qi.somecastapp.utilities.NetworkUtils;
+
+/**
+ * A fragment representing a list of Items.
+ */
 public class DiscoverFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private static final String TAG = DiscoverFragment.class.getSimpleName();
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private RequestQueue requestQueue;
+    private GenreListAdapter genreAdapter;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
     public DiscoverFragment() {
     }
 
-    public static DiscoverFragment getInstance(String param1, String param2) {
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static DiscoverFragment newInstance(int columnCount) {
         DiscoverFragment fragment = new DiscoverFragment();
         Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+        requestQueue = Volley.newRequestQueue(getContext());
+        Response.Listener<String> responseListener = new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response);
+                genreAdapter.setData(JsonUtils.parseGenres(response));
+            }
+        };
+        requestQueue.add(NetworkUtils.getGenreList(responseListener));
     }
 
-    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_discover_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            genreAdapter = new GenreListAdapter(requestQueue);
+            recyclerView.setAdapter(genreAdapter);
+        }
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
 }
--- app/src/main/java/com/qi/somecastapp/MainActivity.java
+++ app/src/main/java/com/qi/somecastapp/MainActivity.java
@@ -5,11 +5,9 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_discover:
                     toolbar.setTitle(R.string.title_discover);
-                    fragment = new DiscoverFragment2();
+                    fragment = new DiscoverFragment();
                     loadFragment(fragment);
                     return true;
                 case R.id.navigation_downloads:

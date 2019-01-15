5dd7bac6ed9777046c87afabdf3360d6c5fdbe0f
Qi Wu <wuqi.vortex@gmail.com>
2019-01-14 23:45:16 -0500

    widget added

---
+++ app/src/main/java/com/qi/somecastapp/widget/SubscriptionList.java
@@ -0,0 +1,48 @@
+package com.qi.somecastapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.qi.somecastapp.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SubscriptionList extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.subscription_list);
+        Intent intent = new Intent(context, SubscriptionWidgetService.class);
+//        intent.setData(Uri.fromParts("content", String.valueOf(appWidgetId), null));
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        views.setRemoteAdapter(R.id.widget_list_view, intent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
---
+++ app/src/main/java/com/qi/somecastapp/widget/SubscriptionWidgetService.java
@@ -0,0 +1,91 @@
+package com.qi.somecastapp.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.qi.somecastapp.R;
+import com.qi.somecastapp.model.Podcast;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+class SubscriptionWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        int widgetId = intent.getData() != null ?
+                Integer.valueOf(intent.getData().getSchemeSpecificPart())
+                : AppWidgetManager.INVALID_APPWIDGET_ID;
+        return new ListRemoteViewsFactory(this.getApplicationContext(), widgetId);    }
+}
+
+class ListRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
+
+    private Context mContext;
+    private BroadcastReceiver mIntentListener;
+    private int widgetId;
+    private ArrayList<Podcast> list = new ArrayList<>();
+
+
+    ListRemoteViewsFactory(Context mContext, int id) {
+        this.mContext = mContext;
+        widgetId = id;
+    }
+
+    @Override
+    public void onCreate() {
+    }
+
+    @Override
+    public void onDataSetChanged() {
+//        list = loadListPref(mContext, widgetId);
+    }
+
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int i) {
+        RemoteViews views = new RemoteViews(mContext.getPackageName(), R.layout.podcast_search_result_holder);
+        views.setTextViewText(R.id.tv_show_name, list.get(i).getPodcastName());
+        try {
+            Bitmap b = Picasso.with(mContext).load(list.get(i).getImagePath()).get();
+            views.setImageViewBitmap(R.id.iv_poster, b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return views;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+}
